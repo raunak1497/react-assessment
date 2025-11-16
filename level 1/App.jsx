28d59f47848519f5d80a6ae2abd49efedc931738
RAUNAK SINGH <rsing76@uic.edu>
2025-11-15 18:11:17 -0600

    level 1 completed

--- level 1/App.jsx
+++ level 1/App.jsx
@@ -2,17 +2,15 @@ import './index.scss';
 import TaskColumn from './TaskColumn';
 import taskData from './taskData.json';
 
-const tasks = taskData.tasks;
-
 const App = () => {
   return (
     <main>
       <div className="board">
         <h2 className="board__title">Tasks</h2>
         <div className="board__columns">
-          <TaskColumn key="toDoColumn" title="To Do"  />
-          <TaskColumn key="inProgressColumn" title="In Progress" />
-          <TaskColumn key="doneColumn" title="Done" />
+          <TaskColumn key="toDoColumn" title="To Do" items={taskData.todoItems}/>
+          <TaskColumn key="inProgressColumn" title="In Progress" items={taskData.inProgressItems}/>
+          <TaskColumn key="doneColumn" title="Done"items={taskData.doneItems}/>
         </div>
       </div>
     </main>
--- level 1/TaskColumn.jsx
+++ level 1/TaskColumn.jsx
@@ -1,8 +1,22 @@
-const TaskColumn = ({ title }) => (
-  <div className="column">
-    <h2 className="column__title">{title}</h2>
-    <div className="column__cards"></div>
-  </div>
-);
+import TaskCard from "./TaskCard";
+
+const TaskColumn = ({ title, items = [] }) => {
+  return(
+    <div className="column">
+      <h2 className="column__title">{title}</h2>
+      <div className="column__cards">
+        {items.map(({id,title,description,assignedUser}) => (
+          <TaskCard 
+          key={id}
+          title={title}
+          description = {description}
+          assignedUser = {assignedUser}
+          />
+        ))}
+      </div>
+    </div>
+  )
+};
 
 export default TaskColumn;
+
